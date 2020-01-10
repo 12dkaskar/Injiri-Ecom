ca0a17451e229f0a19eeb2097d81c69fc49de235
deeptikaskar <deepti@symboinsurance.com>
2020-01-10 14:32:40 +0530

    added : missing files

---
+++ client-config.js
@@ -0,0 +1,11 @@
+/**
+ * Contains client side configuration options.
+ */
+
+const config = {
+	siteUrl: 'http://testo.local',
+	graphqlUrl: 'http://testo.local/graphql',
+	postCodeLocale: 'IN',
+	authTokenName: 'wp-decoupled/ck_b0058f50f7221d2d9bf144f516934c6b3e6cd0e6' ,
+};
+export default config;
